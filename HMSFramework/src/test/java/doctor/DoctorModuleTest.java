a2ffdf0399f5af06e2e01fb560fa6299ff8eb5be
mendon-varsha <mendonvarsha11@gmail.com>
2025-01-06 23:05:12 +0530

    added

---
+++ HMSFramework/src/test/java/doctor/DoctorModuleTest.java
@@ -0,0 +1,13 @@
+package doctor;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DoctorModuleTest {
+@Test
+public void createDoctor()
+{
+	Reporter.log("Doctor details added",true);
+}
+
+}
